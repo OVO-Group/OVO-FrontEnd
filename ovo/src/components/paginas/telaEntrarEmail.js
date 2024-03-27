0990314661dc222860ab2541c7d3e77ad3a9f5f8
Gabriel Duarte <110699790+gduartx@users.noreply.github.com>
2024-03-27 00:37:13 -0300

    Front validação de login por email

--- ovo/src/components/paginas/telaEntrarEmail.js
+++ ovo/src/components/paginas/telaEntrarEmail.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react"
-import Input from "../elementos/Input"
-import BotaoEnviarcodigo from "../eventos/botaoEnviarCodigo"
-import { useNavigate } from "react-router-dom"
+import React, { useState } from "react";
+import Input from "../elementos/Input";
+import BotaoEnviarcodigo from "../eventos/botaoEnviarCodigo";
+import { useNavigate } from "react-router-dom";
 
 function TelaEntrarEmail() {
-    const navigate = useNavigate()
-    const [email, setEmail] = useState('')
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
 
-    function EnviarCodigo() {
-        alert(`Email digitado: ${email}`)
-        navigate('/codigo-email')
+    async function EnviarCodigo() {
+        try {
+            const requestBody = { email: email };
+            const response = await fetch('http://127.0.0.1:8000/login/email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(requestBody),
+            });
+            const data = await response.json();
+            console.log(data);
+            navigate('/codigo-email');
+        } catch (error) {
+            console.error('Erro ao enviar requisição:', error);
+        }
     }
 
-    return(
+    return (
         <div className="card">
             <h1>Informe seu e-mail:</h1>
             <Input 
@@ -25,7 +38,7 @@ function TelaEntrarEmail() {
             />
             <BotaoEnviarcodigo event={EnviarCodigo} text='Enviar'/>
         </div>
-    )
+    );
 }
 
-export default TelaEntrarEmail
+export default TelaEntrarEmail;
